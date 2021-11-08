64bfbdfbc6c3ce66ac1c9e1e7e8aa763be75a02d
burak <binan@quintor.nl>
2021-11-08 13:31:05 +0100

    Re-add persistence

--- src/main/java/meteor/web/service/RegionService.kt
+++ src/main/java/meteor/web/service/RegionService.kt
@@ -1,14 +1,14 @@
 package meteor.web.service
 
 import meteor.web.model.TileFlag
-//import meteor.web.repository.RegionRepository
+import meteor.web.repository.RegionRepository
 import org.springframework.stereotype.Service
 
 @Service
 class RegionService(
-//    val regionRepository: RegionRepository
+    val regionRepository: RegionRepository
 ) {
     fun saveAll(tileFlags: List<TileFlag>) {
-//        regionRepository.saveAll(tileFlags)
+        regionRepository.saveAll(tileFlags)
     }
 }
