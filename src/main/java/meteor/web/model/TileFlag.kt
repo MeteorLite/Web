84937664d9b7d0717f3d983a702e9abc9d5e9d07
burak <binan@quintor.nl>
2021-11-09 22:37:47 +0100

    idk

--- src/main/java/meteor/web/model/TileFlag.kt
+++ src/main/java/meteor/web/model/TileFlag.kt
@@ -1,15 +1,14 @@
 package meteor.web.model
 
-import com.fasterxml.jackson.annotation.JsonProperty
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document
 class TileFlag(
-    @JsonProperty @Id val regionCoords: String,
-    @JsonProperty val x: Int,
-    @JsonProperty val y: Int,
-    @JsonProperty val z: Int,
-    @JsonProperty val flag: Int,
-    @JsonProperty val region: Int,
+    @Id val regionCoords: String?,
+    val x: Int?,
+    val y: Int?,
+    val z: Int?,
+    val flag: Int?,
+    val region: Int?,
 )
