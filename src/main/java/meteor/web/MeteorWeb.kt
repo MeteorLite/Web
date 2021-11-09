5b884d84f04b84f93fb0d005d4f72e9e363ca1b5
burak <binan@quintor.nl>
2021-11-09 22:48:54 +0100

    idk

--- src/main/java/meteor/web/MeteorWeb.kt
+++ src/main/java/meteor/web/MeteorWeb.kt
@@ -1,12 +1,9 @@
 package meteor.web
 
-import meteor.web.repository.RegionRepository
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 
 @SpringBootApplication
-@EnableMongoRepositories(basePackageClasses = [RegionRepository::class])
 class MeteorWeb
 
 fun main(args: Array<String>) {
