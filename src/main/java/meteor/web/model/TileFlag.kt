a2a9503717e076b5284f027470c3f0bca1bfc4e6
burak <binan@quintor.nl>
2021-11-09 23:34:24 +0100

    test

--- src/main/java/meteor/web/model/TileFlag.kt
+++ src/main/java/meteor/web/model/TileFlag.kt
@@ -1,14 +1,14 @@
 package meteor.web.model
 
-import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document
 class TileFlag(
-    @Id val regionCoords: String?,
     val x: Int?,
     val y: Int?,
     val z: Int?,
     val flag: Int?,
     val region: Int?,
+    val regionX: Int?,
+    val regionY: Int?,
 )
