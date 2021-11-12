971767e6064ed7f580abae770b3b24950fa3fb19
burak <binan@quintor.nl>
2021-11-12 11:48:27 +0100

    Fix collision detection

--- src/main/java/meteor/web/model/TileFlag.kt
+++ src/main/java/meteor/web/model/TileFlag.kt
@@ -11,4 +11,32 @@ class TileFlag(
     val region: Int?,
     val regionX: Int?,
     val regionY: Int?,
-)
+) {
+    companion object {
+        enum class Direction(val flag: Int) {
+            NORTH(0x2),
+            WEST(0x80),
+            SOUTH(0x20),
+            EAST(0x8)
+        }
+    }
+    fun check(checkFlag: Int): Boolean {
+        return flag!! and checkFlag != 0
+    }
+
+    fun isWalled(direction: Direction): Boolean {
+        if (flag == 0) {
+            return false
+        }
+
+        return check(direction.flag)
+    }
+
+    fun isObstacle(): Boolean {
+        if (flag == 0) {
+            return false
+        }
+
+        return check(0x100 or 0x20000 or 0x200000 or 0x1000000)
+    }
+}
