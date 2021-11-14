9bd103eadbaf77504fd69746947c0304dcbb8461
Burak <burakinan1@hotmail.com>
2021-11-14 22:10:52 +0100

    Non jpa version

--- src/main/java/meteor/web/controller/RegionController.kt
+++ src/main/java/meteor/web/controller/RegionController.kt
@@ -1,37 +1,18 @@
 package meteor.web.controller
 
 import meteor.web.collisions.CollisionMap
-import meteor.web.config.RegionConfig
 import meteor.web.model.TileFlag
-import meteor.web.repository.RegionRepository
 import org.springframework.core.io.FileSystemResource
-import org.springframework.core.io.InputStreamResource
-import org.springframework.core.io.Resource
 import org.springframework.http.MediaType
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import org.springframework.web.multipart.MultipartFile
-import java.io.ByteArrayInputStream
-import java.io.File
-import java.io.FileInputStream
 import java.nio.file.Files
-import java.nio.file.Path
-import java.util.zip.GZIPInputStream
 
 @RestController
 @RequestMapping("regions")
 class RegionController(
     private val collisionMap: CollisionMap,
-    private val regionRepository: RegionRepository,
 ) {
     val dbVersion = 1
-
-    @GetMapping("/{x}/{y}/{z}")
-    fun getTile(@PathVariable x: Int, @PathVariable y: Int, @PathVariable z: Int) =
-        regionRepository.findFirstByXAndYAndZ(x, y, z)
-
-    @GetMapping("/{regionId}")
-    fun getByRegionId(@PathVariable regionId: Int) = regionRepository.findByRegion(regionId)
 
     @PostMapping("/{version}")
     fun saveAll(@PathVariable version: Int, @RequestBody tiles: List<TileFlag>) {
@@ -66,8 +47,6 @@ class RegionController(
         collisionMap.writeToFile().also { newFile ->
             Files.write(newFile.toPath(), newFile.readBytes())
         }
-
-        regionRepository.saveAll(tiles)
     }
 
     @GetMapping(produces = [MediaType.APPLICATION_OCTET_STREAM_VALUE])
